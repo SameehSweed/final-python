837e6fb8fa1896db5388bffb707611e247822839
sameeh sweed <88625429+SameehSweed@users.noreply.github.com>
2021-09-09 12:50:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from modules.main_functions import *
+
+# ############################# #
+#   Done by :                   #
+#   Median Nabwani - 207571498  #
+#   Sameeh Sweed - 206444739    #
+#   Class 45/1                  #
+# ############################# #
+run_main()
+
+# C:\Users\Median\Desktop\median2.txt
